33ff23326927799fc487a27d5f17d2cd7203c2a5
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-24 14:02:09 +0300

    deploy

--- partials/head.php
+++ partials/head.php
@@ -4,6 +4,7 @@
 <?php seo() ?>
 <meta name="yandex-verification" content="d2f5c2286aca6f1b" />
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
+<meta name="yandex-verification" content="ea94e583da7663a5" />
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" type="text/css" />
 <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
