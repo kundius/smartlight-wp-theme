a8847096f4dedab3dbf0ce83d99d1d1d1bc04a72
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-24 19:23:57 +0300

    deploy

--- sitemap.php
+++ sitemap.php
@@ -20,61 +20,63 @@ wp_enqueue_script('theme_contacts', get_template_directory_uri() . '/dist/sitema
           </div>
           <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
           <h1 class="sitemap-headline"><span><?php the_title() ?></span></h1>
-          <div class="sitemap-grid">
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li><a href="<?php the_permalink(2) ?>">Главная</a></li>
-                <li><a href="<?php the_permalink(7) ?>">О компании</a></li>
-                <li><a href="<?php the_permalink(39) ?>">Наши работы</a></li>
-                <li><a href="<?php the_permalink(11) ?>">Цены</a></li>
-                <li><a href="<?php the_permalink(9) ?>">Контакты</a></li>
-              </ul>
-            </div>
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li class="_opened">
-                  <a href="<?php the_permalink(85) ?>">Освещение</a>
-                  <ul class="sitemap-list">
-                    <li class="_opened">
-                      <a href="#">Новогоднее освещение</a>
-                      <ul class="sitemap-list">
-                        <li><a href="<?php the_permalink(341) ?>">Освещение домов, коттеджей</a></li>
-                        <li><a href="<?php the_permalink(287) ?>">Освещение зданий и объектов</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="<?php the_permalink(160) ?>">Архитектурное освещение</a></li>
-                    <li><a href="<?php the_permalink(87) ?>">Уличное освещение</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li>
-                  <a href="<?php the_permalink(76) ?>">Акции</a>
-                </li>
-              </ul>
-            </div>
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li>
-                  <a href="<?php the_permalink(45) ?>">Статьи</a>
-                </li>
-              </ul>
-            </div>
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li>
-                  <a href="<?php the_permalink(360) ?>">Пользовательское соглашение</a>
-                </li>
-              </ul>
-            </div>
-            <div class="sitemap-grid__cell">
-              <ul class="sitemap-list">
-                <li>
-                  <a href="<?php the_permalink(3) ?>">Политика конфиденциальности и обработкиперсональных данных</a>
-                </li>
-              </ul>
+          <div class="sitemap-body">
+            <div class="sitemap-grid">
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li><a href="<?php the_permalink(2) ?>">Главная</a></li>
+                  <li><a href="<?php the_permalink(7) ?>">О компании</a></li>
+                  <li><a href="<?php the_permalink(39) ?>">Наши работы</a></li>
+                  <li><a href="<?php the_permalink(11) ?>">Цены</a></li>
+                  <li><a href="<?php the_permalink(9) ?>">Контакты</a></li>
+                </ul>
+              </div>
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li class="_opened">
+                    <a href="<?php the_permalink(85) ?>">Освещение</a>
+                    <ul class="sitemap-list">
+                      <li class="_opened">
+                        <a href="#">Новогоднее освещение</a>
+                        <ul class="sitemap-list">
+                          <li><a href="<?php the_permalink(341) ?>">Освещение домов, коттеджей</a></li>
+                          <li><a href="<?php the_permalink(287) ?>">Освещение зданий и объектов</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="<?php the_permalink(160) ?>">Архитектурное освещение</a></li>
+                      <li><a href="<?php the_permalink(87) ?>">Уличное освещение</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li>
+                    <a href="<?php the_permalink(76) ?>">Акции</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li>
+                    <a href="<?php the_permalink(45) ?>">Статьи</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li>
+                    <a href="<?php the_permalink(360) ?>">Пользовательское соглашение</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="sitemap-grid__cell">
+                <ul class="sitemap-list">
+                  <li>
+                    <a href="<?php the_permalink(3) ?>">Политика конфиденциальности и обработкиперсональных данных</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <?php endwhile; else: ?>
