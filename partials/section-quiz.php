8d20a3b388355beb60d9cdd50caf5df676336e4d
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-25 05:39:58 +0300

    deploy

--- partials/section-quiz.php
+++ partials/section-quiz.php
@@ -4,10 +4,31 @@
   <div class="container">
     <div class="quiz">
       <div class="quiz__title">
-        Получите <span>скидку на монтаж 10% </span> и расчет стоимости, ответив на 5 простых вопросов
+        Получите <span>скидку на монтаж 10% </span> и&nbsp;расчет стоимости, ответив на&nbsp;5&nbsp;простых вопросов
       </div>
-      <div class="quiz-decoration-1"></div>
+      <div class="quiz-decoration-1-1"></div>
+      <div class="quiz-decoration-1-2"></div>
+      <div class="quiz-decoration-1-3"></div>
+      <div class="quiz-decoration-1-4"></div>
+      <div class="quiz-decoration-1-5"></div>
+      <div class="quiz-decoration-1-6"></div>
+      <div class="quiz-decoration-1-7"></div>
+      <div class="quiz-decoration-1-8"></div>
+      <div class="quiz-decoration-1-9"></div>
       <div class="quiz-decoration-2"></div>
+      <div class="quiz-decoration-3"></div>
+      <div class="quiz-decoration-4-1"></div>
+      <div class="quiz-decoration-4-2"></div>
+      <div class="quiz-decoration-4-3"></div>
+      <div class="quiz-decoration-4-4"></div>
+      <div class="quiz-decoration-5"></div>
+      <div class="quiz-decoration-6"></div>
+      <div class="quiz-decoration-7"></div>
+      <div class="quiz-decoration-8"></div>
+      <div class="quiz-decoration-9"></div>
+      <div class="quiz-decoration-10"></div>
+      <div class="quiz-decoration-11"></div>
+      <div class="quiz-decoration-12"></div>
       <div class="quiz-body">
         <div class="quiz-body__left">
           <br>
