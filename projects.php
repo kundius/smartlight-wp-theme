32e39a0c96376b4d18cf8c87d2a015cfe0762e76
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-27 23:46:54 +0300

    deploy

--- projects.php
+++ projects.php
@@ -5,14 +5,16 @@ Template Name: Проекты
 wp_enqueue_script('theme_projects', get_template_directory_uri() . '/dist/projects.js', ['theme_common'], false, true);
 
 global $wp_query;
+$terms = get_query_var('terms') ? get_query_var('terms') : null;
+print_r($terms);
 $projects = new WP_Query(array(
   'post_type' => 'project',
   'posts_per_page' => 27,
   'paged' => get_query_var('paged') ?: 1,
-	// 'tax_query' => [[
+	// 'tax_query' => $terms ? [[
   //   'taxonomy' => 'project_category',
-  //   'terms' => []
-  // ]]
+  //   'terms' => $terms
+  // ]] : null
 ));
 ?>
 <!DOCTYPE html>
