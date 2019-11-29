b96404715e92676452180b14122d8c889862a685
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-29 15:10:12 +0300

    deploy

--- src/js/common.js
+++ src/js/common.js
@@ -218,7 +218,7 @@ forEach(document.querySelectorAll('[data-slider]'), function(slider) {
     } else {
       timeline.add(progress => {
         elItems.forEach((slide, i) => {
-          slide.style.order = i < active ? -1 : -2
+          slide.style.order = i < active ? -2 : -1
         })
         if (params.vertical) {
           let height = elItems[active].offsetHeight
