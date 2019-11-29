2c273d3fe40346749eb317d2c970968c8824c965
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-29 18:33:20 +0300

    deploy

--- src/js/common.js
+++ src/js/common.js
@@ -251,7 +251,7 @@ forEach(document.querySelectorAll('[data-slider]'), function(slider) {
             elWrapper.style.transform = `translate3d(-${width - (width * progress)}px, 0px, 0px)`
           }
         }
-        console.log('взад', active, dist, k)
+        console.log('взад', active, dist, k, active - (active - dist + k))
         timeline.add(callback.bind(this, retreat, active - (active - dist + k)))
       }
     }
