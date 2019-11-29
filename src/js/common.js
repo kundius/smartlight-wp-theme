e20864bb9f5ce7eb04bd5a9d026b586347a06239
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-11-29 18:46:17 +0300

    deploy

--- src/js/common.js
+++ src/js/common.js
@@ -227,9 +227,8 @@ forEach(document.querySelectorAll('[data-slider]'), function(slider) {
             elWrapper.style.transform = `translate3d(-${width * progress}px, 0px, 0px)`
           }
         }
-
-        console.log('вперед', active, dist, k, Math.abs(active + k - dist))
-        timeline.add(callback.bind(this, retreat, Math.abs(active + k - dist)))
+        console.log('вперед', active, dist, k, k)
+        timeline.add(callback.bind(this, retreat, k))
       } else {
         let callback = (retreat, active, progress) => {
           elItems.forEach((slide, i) => {
