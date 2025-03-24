cde209e3bf1f980ba80d79e0184745114340b1af
Kundius Ruslan <kundius.ruslan@gmail.com>
2025-03-24 23:37:25 +0300

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -6,6 +6,7 @@
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
 <meta name="yandex-verification" content="ea94e583da7663a5" />
 <meta name="yandex-verification" content="8218385e7ebf8200" />
+<meta name="yandex-verification" content="c382e500025f1ae5" />
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" type="text/css" />
 <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
